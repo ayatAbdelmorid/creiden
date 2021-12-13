94e47f52e830b1b6331c1c1921681b3573e50ea9
ayatAbdelmorid <ayatabdelmorid@gmail.com>
2021-12-13 20:51:11 +0200

    fix validate admin

--- app/Http/Controllers/AuthAdmin/AuthController.php
+++ app/Http/Controllers/AuthAdmin/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:admins',
             'password' => 'required|string|min:8|confirmed',
             'password_confirmation' => 'required',
         ]);
